06a191b90219524824d3b552972bd15464c19131
Sasha Belpois Baticle <sasha.belpois@gmail.com>
2025-02-13 19:27:51 +0100

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,5 @@ user3.save!
 
 
 
-
-
-
-
-file = URI.parse("https://www.net-a-porter.com/variants/images/1647597343734696/in/w2000_q60.jpg").open
 clothe1 = Clothe.new(type: "haut", color: "blue", size: "s", genre: "homme", price_per_day: "25", user_id: user1.id )
-clothe1.photo.attach(io: file, filename: "clothe1.png", content_type: "image/png")
-clothe1.save
+clothe1.save!
