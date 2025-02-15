2f380caff201d7a96e022e3055820f1ba9c08981
Sasha Belpois Baticle <sasha.belpois@gmail.com>
2025-02-15 10:53:53 +0100

    commit before pull

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -2,4 +2,24 @@ class ClothesController < ApplicationController
   def show
     @clothe = Clothe.find(params[:id])
   end
+
+  def new
+    @clothe = Clothe.new
+  end
+
+  def create
+    @clothe = Clothe.new(clothe_params)
+    @clothe.user = current_user
+    if @clothe.save
+      redirect_to clothes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def clothe_params
+    params.require(:clothe).permit(:color, :price_per_day, :clothe_type, :genre, :size)
+  end
 end
