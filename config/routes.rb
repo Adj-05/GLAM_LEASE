83984c9723d370d15d5746b835678e650272ed20
Aurelie Djoki <aureliedjoki@gmail.com>
2025-02-20 20:58:01 +0100

    homepage set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "clothes#index"
+
+  root to: "home#index"
+
+  #resources :clothes, only: [:index, :show]
 
   resources :clothes do
     resources :bookings, only: [:new, :create]
