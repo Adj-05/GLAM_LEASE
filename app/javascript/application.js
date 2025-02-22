92fca2eb17625c56f298d91d43d3c58e7dcd92ff
Sasha Belpois Baticle <sasha.belpois@gmail.com>
2025-02-22 16:06:57 +0100

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,28 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import "./image_preview";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const photoInput = document.getElementById("photo_input");
+  const photoPreview = document.getElementById("photo_preview");
+
+
+
+  if (!photoInput || !photoPreview) {
+    return;
+  }
+  photoInput.addEventListener("change", function (event) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        photoPreview.src = e.target.result;
+        photoPreview.style.display = "block";
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
+});
